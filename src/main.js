3164f500d91c82ea133a76e5d94ee0bef9927f36
abdisetiakawan <bedikadiryt@gmail.com>
2025-03-05 11:40:42 +0700

    chore: Updates project dependencies and adjusts paths
    Updates Bootstrap and Bootstrap Icons versions.
    Refactors paths to use relative paths for local dependencies.
    Includes Bootstrap JS bundle for improved functionality.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
