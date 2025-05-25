f6a3c9c4499f2d7dce0bb8116f0ec26715c35537
abdisetiakawan <bedikadiryt@gmail.com>
2025-05-25 21:21:24 +0700

    feat: Integrates Bootstrap for styling
    Adds Bootstrap CSS and JS to the project for improved styling and UI components.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
+// Import Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
